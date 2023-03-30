d8a62d0aa9ff57cba59c1357e09fd84c1bdc7589
Xyndra <sammy@deutschergamingserver.de>
2023-03-30 20:28:53 +0200

    Trying to fix the GitHub Page #2

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import svelte from "@astrojs/svelte";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xyndra.github.io',
+  base: '/math_renderer',
   integrations: [svelte()]
 });
